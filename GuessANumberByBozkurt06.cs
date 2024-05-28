92a12346f5ee030589a70b350991e0ce344a2ae8
Bozkurt06 <reyzarpro@gmail.com>
2024-05-28 14:16:55 +0300

    Uploading a secound project. It s a secound game GuessANumberByBozkurt06

---
+++ GuessANumberByBozkurt06.cs
@@ -0,0 +1,29 @@
+﻿Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Gues a number (1-100): ");
+    string playerInput = Console.ReadLine();
+    bool isValid = int.TryParse(playerInput, out int playerNumber);
+    if (isValid)
+    {
+        if (playerNumber == computerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNumber > computerNumber)
+        {
+            Console.WriteLine("To high");
+        }
+        else
+        {
+            Console.WriteLine("To low");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+}
